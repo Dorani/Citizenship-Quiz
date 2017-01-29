c59e83e33a9ec6a6aec25687690b9761a85289fa
Seif <seif.dorani@gmail.com>
2017-01-29 16:56:36 -0500

    questions array created, with choices and answers in specific indecies

---
+++ app.js
@@ -0,0 +1,6 @@
+// Creating questions
+
+var questions = [
+  new Question("Who was the 1st president?" , ["GW", "Thomas Jeff"], "GW"),
+  new Question("How many stars on the flag?") , ["1", "10", "50"], "50");
+];
