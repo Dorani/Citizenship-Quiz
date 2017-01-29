c713a578c01e65ebce3917b2f971deeab7dfe1ef
Seif <seif.dorani@gmail.com>
2017-01-29 16:49:30 -0500

    w2 methods added

--- quiz.js
+++ quiz.js
@@ -4,9 +4,18 @@ function Quiz(questions){
   this.currentQuestionIndex = 0;
 }
 
+
 Quiz.prototype.guess = function(answer){
   if(this.getCurrentQuestion().isCorrectanswer(answer)){
     this.score++;
   }
   this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function(){
+  return this.questions[this.currentQuestionIndex];
+ };
+
+Quiz.prototype.hasEnded = function(){
+  return this.currentQuestionIndex >= this.questions.length;
 };
