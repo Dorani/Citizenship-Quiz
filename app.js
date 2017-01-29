eb49d3d822125bc722a6f4655d71b7a98e96c624
Seif <seif.dorani@gmail.com>
2017-01-29 16:58:07 -0500

    initialize quiz with question arr

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ var questions = [
   new Question("Who was the 1st president?" , ["GW", "Thomas Jeff"], "GW"),
   new Question("How many stars on the flag?") , ["1", "10", "50"], "50");
 ];
+
+// Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quiz
+QuizUI.displayNext();
