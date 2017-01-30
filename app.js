b28db7d30052b8274cdfd39cb405247726edfb1d
Seif <seif.dorani@gmail.com>
2017-01-30 02:05:27 -0500

    fixed ui

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 var questions = [
   new Question("Who was the 1st president?" , ["GW", "Thomas Jeff"], "GW"),
-  new Question("How many stars on the flag?" , ["1", "10", "50"], "50")
+  new Question("How many stars on the flag?" , ["1", "50"], "50")
+
 ];
 
 // Create Quiz
