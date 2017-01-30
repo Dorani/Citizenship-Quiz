cadbfd27ec0eeec122b6a6fd54978ed640ad22fa
Seif <seif.dorani@gmail.com>
2017-01-30 02:19:28 -0500

    adding more content

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var questions = [
   new Question("What did Abraham Lincoln do?" , ["freed the slaves", "pimped rides like exibit"], "freed the slaves"),
   new Question("What are 2 parts of congress?" , ["the senate and house", "the pizza joint"], "the senate and house"),
   new Question("We elect president for how many years?" , ["4", "2"], "4"),
+  new Question("Why did the colonies fight the brits?" , ["they were bored", "taxes"], "taxes"),
+  new Question("What is the supreme law of the land?" , ["constitution", "bible"], "constitution"),
+
 ];
 
 // Create Quiz
